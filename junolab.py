a908cf80a9f230c1336ebe9fb285a8a516fbfe1a
Ivar Farup <ivar.farup@ntnu.no>
2019-03-01 12:10:20 +0100

    refactored slots for sliders and radio buttons

--- junolab.py
+++ junolab.py
@@ -26,98 +26,53 @@ class JunoLab(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # Connect up the controls
-        self.vcf_hpf_slider.valueChanged.connect(self.vcf_hpf_slot)
-        self.vcf_freq_slider.valueChanged.connect(self.vcf_freq_slot)
-        self.vcf_res_slider.valueChanged.connect(self.vcf_res_slot)
-        self.vcf_lfo_slider.valueChanged.connect(self.vcf_lfo_slot)
-        self.vcf_key_flw_slider.valueChanged.connect(self.vcf_key_flw_slot)
-        self.vcf_after_slider.valueChanged.connect(self.vcf_after_slot)
-        self.vcf_env_slider.valueChanged.connect(self.vcf_env_slot)
-        self.vcf_env_mode_env_rbutton.pressed.connect(self.vcf_env_mode_env_slot)
-        self.vcf_env_mode_inv_rbutton.pressed.connect(self.vcf_env_mode_inv_slot)
-        self.vcf_env_mode_denv_rbutton.pressed.connect(self.vcf_env_mode_denv_slot)
-        self.vcf_env_mode_dyn_rbutton.pressed.connect(self.vcf_env_mode_dyn_slot)
+        self.vcf_hpf_slider.valueChanged.connect(
+            lambda: self.on_slider_change(9, self.vcf_hpf_slider))
+        self.vcf_freq_slider.valueChanged.connect(
+            lambda: self.on_slider_change(16, self.vcf_freq_slider))
+        self.vcf_res_slider.valueChanged.connect(
+            lambda: self.on_slider_change(17, self.vcf_res_slider))
+        self.vcf_lfo_slider.valueChanged.connect(
+            lambda: self.on_slider_change(18, self.vcf_lfo_slider))
+        self.vcf_key_flw_slider.valueChanged.connect(
+            lambda: self.on_slider_change(20, self.vcf_key_flw_slider))
+        self.vcf_after_slider.valueChanged.connect(
+            lambda: self.on_slider_change(21, self.vcf_after_slider))
+        self.vcf_env_slider.valueChanged.connect(
+            lambda: self.on_slider_change(19, self.vcf_env_slider))
+        self.vcf_env_mode_env_rbutton.pressed.connect(
+            lambda: self.on_button_press(1, 0))
+        self.vcf_env_mode_inv_rbutton.pressed.connect(
+            lambda: self.on_button_press(1, 1))
+        self.vcf_env_mode_denv_rbutton.pressed.connect(
+            lambda: self.on_button_press(1, 2))
+        self.vcf_env_mode_dyn_rbutton.pressed.connect(
+            lambda: self.on_button_press(1, 3))
 
-        self.env_t1_slider.valueChanged.connect(self.env_t1_slot)
-        self.env_l1_slider.valueChanged.connect(self.env_l1_slot)
-        self.env_t2_slider.valueChanged.connect(self.env_t2_slot)
-        self.env_l2_slider.valueChanged.connect(self.env_l2_slot)
-        self.env_t3_slider.valueChanged.connect(self.env_t3_slot)
-        self.env_l3_slider.valueChanged.connect(self.env_l3_slot)
-        self.env_t4_slider.valueChanged.connect(self.env_t4_slot)
-        self.env_key_flw_slider.valueChanged.connect(self.env_key_flw_slot)
+        self.env_t1_slider.valueChanged.connect(
+            lambda: self.on_slider_change(26, self.env_t1_slider))
+        self.env_l1_slider.valueChanged.connect(
+            lambda: self.on_slider_change(27, self.env_l1_slider))
+        self.env_t2_slider.valueChanged.connect(
+            lambda: self.on_slider_change(28, self.env_t2_slider))
+        self.env_l2_slider.valueChanged.connect(
+            lambda: self.on_slider_change(29, self.env_l2_slider))
+        self.env_t3_slider.valueChanged.connect(
+            lambda: self.on_slider_change(30, self.env_t3_slider))
+        self.env_l3_slider.valueChanged.connect(
+            lambda: self.on_slider_change(31, self.env_l3_slider))
+        self.env_t4_slider.valueChanged.connect(
+            lambda: self.on_slider_change(32, self.env_t4_slider))
+        self.env_key_flw_slider.valueChanged.connect(
+            lambda: self.on_slider_change(33, self.env_key_flw_slider))
 
-    def vcf_hpf_slot(self):
-        val = self.vcf_hpf_slider.value()
-        sysex_send_ipr(9, val)
+    # Slots
 
-    def vcf_freq_slot(self):
-        val = self.vcf_freq_slider.value()
-        sysex_send_ipr(16, val)
+    def on_slider_change(self, parameter, slider):
+        sysex_send_ipr(parameter, slider.value())
 
-    def vcf_res_slot(self):
-        val = self.vcf_res_slider.value()
-        sysex_send_ipr(17, val)
-
-    def vcf_lfo_slot(self):
-        val = self.vcf_lfo_slider.value()
-        sysex_send_ipr(18, val)
-
-    def vcf_key_flw_slot(self):
-        val = self.vcf_key_flw_slider.value()
-        sysex_send_ipr(20, val)
-
-    def vcf_after_slot(self):
-        val = self.vcf_after_slider.value()
-        sysex_send_ipr(21, val)
-
-    def vcf_env_slot(self):
-        val = self.vcf_env_slider.value()
-        sysex_send_ipr(19, val)
-
-    def vcf_env_mode_env_slot(self):
-        sysex_send_ipr(1, 0)
-
-    def vcf_env_mode_inv_slot(self):
-        sysex_send_ipr(1, 1)
-
-    def vcf_env_mode_denv_slot(self):
-        sysex_send_ipr(1, 2)
-
-    def vcf_env_mode_dyn_slot(self):
-        sysex_send_ipr(1, 3)
-
-    def env_t1_slot(self):
-        val = self.env_t1_slider.value()
-        sysex_send_ipr(26, val)
-
-    def env_l1_slot(self):
-        val = self.env_l1_slider.value()
-        sysex_send_ipr(27, val)
-
-    def env_t2_slot(self):
-        val = self.env_t2_slider.value()
-        sysex_send_ipr(28, val)
-
-    def env_l2_slot(self):
-        val = self.env_l2_slider.value()
-        sysex_send_ipr(29, val)
-
-    def env_t3_slot(self):
-        val = self.env_t3_slider.value()
-        sysex_send_ipr(30, val)
-
-    def env_l3_slot(self):
-        val = self.env_l3_slider.value()
-        sysex_send_ipr(31, val)
-
-    def env_t4_slot(self):
-        val = self.env_t4_slider.value()
-        sysex_send_ipr(32, val)
-
-    def env_key_flw_slot(self):
-        val = self.env_key_flw_slider.value()
-        sysex_send_ipr(33, val)
+    def on_button_press(self, parameter, value):
+        sysex_send_ipr(parameter, value)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
